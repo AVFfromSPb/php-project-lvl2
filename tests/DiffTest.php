96fb6ef9d062449fb35bc9867839d121c2af20fb
Aleksandr Fedorov <a.v.fedorov5@gmail.com>
2021-07-06 17:41:54 +0300

    Add test DiffTest for checking getDiff function

---
+++ tests/DiffTest.php
@@ -0,0 +1,82 @@
+<?php
+namespace Differ\Tests;
+use PHPUnit\Framework\TestCase;
+use function Differ\Diff\getDiff;
+
+class DiffTest extends TestCase
+{
+    public function testGetDiff()
+    {
+        $before = [
+            'host' => 'hexlet.io',
+            'timeout' => 50,
+            'proxy' => '123.234.53.22'
+        ];
+        $after = [
+            'timeout' => 20,
+            'verbose' => true,
+            'host' => 'hexlet.io'
+        ];
+        $expected = [
+            '  host: hexlet.io',
+            '+ timeout: 20',
+            '- timeout: 50',
+            '- proxy: 123.234.53.22',
+            '+ verbose: true'
+        ];
+        $actual = getDiff($before, $after);
+        $this->assertEquals($expected, $actual);
+    }
+
+    public function testGetDiffWithEmptyValue()
+    {
+        $before = [
+            'host' => 'hexlet.io',
+            'timeout' => 50,
+            'proxy' => '123.234.53.22'
+        ];
+        $after = [];
+        $expected = [
+            '- host: hexlet.io',
+            '- timeout: 50',
+            '- proxy: 123.234.53.22'
+        ];
+        $actual = getDiff($before, $after);
+        $this->assertEquals($expected, $actual);
+    }
+
+    public function testGetDiffWithBooleanValue()
+    {
+        $before = [
+            'host' => 'hexlet.io',
+            'timeout' => 50,
+            'proxy' => '123.234.53.22'
+        ];
+        $after = [
+            'timeout' => 0,
+            'verbose' => true,
+            'host' => 'hexlet.io',
+            'result' => false
+        ];
+        $expected = [
+            '  host: hexlet.io',
+            '+ timeout: 0',
+            '- timeout: 50',
+            '- proxy: 123.234.53.22',
+            '+ verbose: true',
+            '+ result: false'
+        ];
+        $actual = getDiff($before, $after);
+        $this->assertEquals($expected, $actual);
+
+    
+          
+            
+    
+
+          
+    
+    
+  
+    }
+}
